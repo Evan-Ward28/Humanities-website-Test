88284b7a6e33528682edea1e18f0775fb37f0fc3
Evan Ward <evanc.ward@outlook.com>
2023-11-05 19:10:56 -0600

    Fixed sources and marked map

--- website.py
+++ website.py
@@ -45,11 +45,30 @@ def hello():
     # Add markers to the MarkerCluster group
     for record in valid_records:
         coord = (record['latitude'], record['longitude'])
-        
-        # Create a pop-up label with multiple fields
-        popup_text = f"Name: {record.get('Name', 'N/A')}<br>Age: {record.get('Age', 'N/A')}<br>Race:{record.get('Race', 'N/A')} {record.get('Gender', 'N/A')}<br>Occupation: {record.get('Occupation', 'N/A')}<br>Crime: {record.get('Crime', 'N/A')}<br>Method: {record.get('Method', 'N/A')}<br>Death: {record.get('Death', 'N/A')}<br>County: {record.get('County', 'N/A')}<br>Source1: {record.get('Source1', 'N/A')}<br>Source2: {record.get('Source2', 'N/A')}<br>Source3: {record.get('Source3', 'N/A')}"
-        
-        marker = folium.Marker(location=coord, popup=popup_text)
+
+        # Create a pop-up label with multiple fields and conditionally include source links
+        popup_text = f"Name: {record.get('Name', '?')}<br>Age: {record.get('Age', '?')}<br>Race:{record.get('Race', '?')} {record.get('Gender', '?')}<br>Occupation: {record.get('Occupation', '?')}<br>Crime: {record.get('Crime', '?')}<br>Method: {record.get('Method', '?')}<br>Death: {record.get('Death', '?')}<br>County: {record.get('County', '?')}<br>"
+
+         # Check for source links and add them to the pop-up if available
+        source_links = []
+
+        if 'Source1' in record and record['Source1']:
+            source_links.append(f"<a href='{record['Source1']}' target='_blank'>Source 1</a>")
+
+        if 'Source2' in record and record['Source2']:
+            source_links.append(f"<a href='{record['Source2']}' target='_blank'>Source 2</a>")
+
+        if 'Source3' in record and record['Source3']:
+            source_links.append(f"<a href='{record['Source3']}' target='_blank'>Source 3</a>")
+
+        if source_links:
+            popup_text += "<br>".join(source_links)
+
+        if popup_text:
+            popup_text += "<br>"
+
+
+        marker = folium.Marker(location=coord, popup=folium.Popup(popup_text, max_width=300))
         marker.add_to(marker_cluster)
         
     # Add the Counties.geojson as a layer to the map
@@ -73,10 +92,13 @@ def hello():
     
     #renders to html
     header = mapObj.get_root().header.render()
-    #
+    #Map render
     body_html = mapObj.get_root().html.render()
     #Java script render
     script =  mapObj.get_root().script.render()
+    
+    
+    #Website render
     return render_template_string( """
     <!DOCTYPE html>
     <html>
@@ -85,7 +107,8 @@ def hello():
         </head>
         <body>
             <h3>State of Tennessee Death Penalty Map </h3>
-            {{body_html|safe}}
+            
+            {{body_html|safe}} <!--Map Render-->
             
             <script>
             {{script|safe}}
